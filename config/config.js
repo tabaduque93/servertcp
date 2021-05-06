5ea2e11dd1acb52444d42aa66f2bf168db627117
tabaduque93 <tadashi.barros@uautonoma.edu.co>
2021-05-06 15:39:38 -0500

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,12 @@
+title = 'nombre ventana cmd'; 
+central_url= 'url de la central';
+puerto_controladores = 0000; // Normalmente después del 9000
+puerto_api = 0000; // Normalmente después del 3000
+
+module.exports = {
+    title,
+    central_url,
+    puerto_controladores,
+    puerto_api
+}
+
